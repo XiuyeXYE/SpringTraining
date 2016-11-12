09aa83287ea2562a964e9b7dc4a5cad66482b7e0
xiuye <xiuye_engineer@outlook.com>
2016-11-12 19:46:37 +0800

    new

--- src/com/xiuye/config/aspect/AspectConfig.java
+++ src/com/xiuye/config/aspect/AspectConfig.java
@@ -2,8 +2,8 @@ package com.xiuye.config.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+/*import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;*/
 
 @Aspect
 //@Configuration
