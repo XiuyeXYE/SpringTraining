86c57ac3387f7383b105d855c4995e18cffde155
xiuye <xiuye_engineer@outlook.com>
2016-11-12 20:00:45 +0800

    new

--- src/com/xiuye/config/aspect/AspectConfig.java
+++ src/com/xiuye/config/aspect/AspectConfig.java
@@ -1,6 +1,10 @@
 package com.xiuye.config.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,6 +26,25 @@ public class AspectConfig {
 	@After("description()")
 	public void afterDescription(){
 		System.out.println("After method:description!");
+	}
+	@AfterReturning("description()")
+	public void afterReturningDescription(){
+		System.out.println("AfterReturning method:description!");
+	}
+	@AfterThrowing("description()")
+	public void afterThrowingDescription(){
+		System.out.println("AfterThrowing method:description!");
+	}
+	@Around("description()")//始终在其通知前面执行
+	public void aroundDescription(ProceedingJoinPoint jp){
+		System.out.println("Around method:description!");
+		System.out.println("Hello World!");
+		try {
+			jp.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		System.out.println("Is game over?!");
 	}
 
 }
