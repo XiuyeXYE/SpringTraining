794699e65a3ba98ecb5116b8583427f8e313fe53
xiuye <xiuye_engineer@outlook.com>
2016-11-29 16:49:12 +0800

    new

--- src/com/xiuye/main/Main.java
+++ src/com/xiuye/main/Main.java
@@ -16,21 +16,22 @@ public class Main {
 
 		String s = DoubleUtil.outputFullLengthDouble("1.67889E10");
 		System.out.println(s);
-//		System.setProperty("test","false");//@Conditional注解对AnnotationConfigApplicationContext不起作用
-//		System.setProperty("spring.profiles.active","dev");//设置(开启)激活的开发环境   可以   不设置报错
-//		//System.setProperty("spring.profiles.default","dev");//设置默认的开发环境  可以
-//
-//		ApplicationContext ac = new AnnotationConfigApplicationContext(BeanConfiguration1.class);
-//		Student s = ac.getBean(Student.class);
-//		System.out.println(s);
-//		s = ac.getBean(Student.class, "Linda","Woman",22,70);
-//		System.out.println(s);
-//		ComponentForStudent cf = ac.getBean(ComponentForStudent.class);
-//		cf.studentInfo();
-//		Component c = ac.getBean(Component.class, "100","200");
-//		System.out.println(c);
-//		Car car = ac.getBean(Car.class);
-//		car.configInfo();
+		System.setProperty("test","false");//@Conditional注解对AnnotationConfigApplicationContext不起作用
+		System.setProperty("spring.profiles.active","dev");//设置(开启)激活的开发环境   可以   不设置报错
+		//System.setProperty("spring.profiles.default","dev");//设置默认的开发环境  可以
+
+		ApplicationContext ac = new AnnotationConfigApplicationContext(BeanConfiguration1.class);
+		Student s1 = ac.getBean(Student.class);
+		System.out.println(s1);
+
+		Student s2 = ac.getBean(Student.class, "Linda","Woman",22,70);
+		System.out.println(s2);
+		ComponentForStudent cf = ac.getBean(ComponentForStudent.class);
+		cf.studentInfo();
+		Component c = ac.getBean(Component.class, "100","200");
+		System.out.println(c);
+		Car car = ac.getBean(Car.class);
+		car.configInfo();
 	}
 
 }
